4a07b53349777e0fcf1ed991e2ae27ec4605e19e
David Ackroyd <davidackroydmusic@gmail.com>
2020-05-02 20:56:48 +0000

    tidied up schema

--- links/schema.py
+++ links/schema.py
@@ -14,3 +14,21 @@ class Query(graphene.ObjectType):
 
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
+
+
+class CreateLink(graphene.Mutation):
+    link = graphene.Field(LinkType)
+
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        return CreateLink(link=link)
+
+
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
