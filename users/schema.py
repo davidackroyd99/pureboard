93c802a40e94ef406d49fe7f531207fc133aeb7b
David Ackroyd <davidackroydmusic@gmail.com>
2020-05-03 09:17:25 +0000

    authentication

--- users/schema.py
+++ users/schema.py
@@ -10,11 +10,18 @@ class UserType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
+    me = graphene.Field(UserType)
     users = graphene.List(UserType)
 
     def resolve_users(self, info):
         return get_user_model().objects.all()
 
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Not logged in!')
+
+        return user
 
 class CreateUser(graphene.Mutation):
     user = graphene.Field(UserType)
