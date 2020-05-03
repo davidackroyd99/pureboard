87ce1443413885126c2e1463a8997b6ae87fdbaa
David Ackroyd <dackroyd@aspidistra.com>
2020-05-03 11:07:08 +0000

    createVote mutation

--- links/schema.py
+++ links/schema.py
@@ -2,7 +2,7 @@ import graphene
 from graphene_django import DjangoObjectType
 
 from .models import Link, Vote
-
+from users.schema import UserType
 
 class LinkType(DjangoObjectType):
     class Meta:
@@ -32,5 +32,32 @@ class CreateLink(graphene.Mutation):
         return CreateLink(link=link)
 
 
+class CreateVote(graphene.Mutation):
+    user = graphene.Field(UserType)
+    link = graphene.Field(LinkType)
+
+    class Arguments:
+        link_id = graphene.Int(required=True)
+        score = graphene.Int()
+
+    def mutate(self, info, link_id, score=1):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('You must be logged to vote!')
+
+        link = Link.objects.filter(id=link_id).first()
+        if not link:
+            raise Exception('Invalid Link!')
+
+        Vote.objects.create(
+            user=user,
+            link=link,
+            score=score
+        )
+
+        return CreateVote(user=user, link=link)
+
+
 class Mutation(graphene.ObjectType):
     create_link = CreateLink.Field()
+    create_vote = CreateVote.Field()
